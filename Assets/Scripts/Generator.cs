176c35822da92221727d1aa1b88e35b5cc7c575b
Envy <middleton.design@outlook.com>
2026-06-04 21:21:39 -0400

    Generator Work
    Set X Value to either 7 or -7

--- Assets/Scripts/Generator.cs
+++ Assets/Scripts/Generator.cs
@@ -1,16 +1,74 @@
 using UnityEngine;
+using UnityEngine.AdaptivePerformance;
 
 public class Generator : MonoBehaviour
-{
+{   //DECLARE VARIABLES
+float timer = 0;
+public GameObject[] EnemyFish;
+public GameObject BubbleShield;
+public GameObject Player;
+Rigidbody2D rB;
+
+Vector3 enemyPosition;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        StopSpawning();
+        rB = GetComponent<Rigidbody2D>();
+        Player = GameObject.Find("Player");
+        enemyPosition = new Vector3(0, 0, 0);
     }
 
     // Update is called once per frame
     void Update()
     {
+        if(timer > 0)
+        {
+            timer -= Time.deltaTime;
+        }
+        else
+        {
+            int chance = Random.Range(1, 101);
+            float posY = Random.Range(-5f, 5f);
+            float posX = Random.Range(-7,7);
+
+            if(chance <= 20)
+            {
+                Instantiate(EnemyFish[1], new Vector3(posX, posY, 0), new Quaternion(0, 0, 0, 0));
+            }
+
+            
+            {
+                Instantiate(EnemyFish[2], new Vector3(posX, posY, 0), new Quaternion(0, 0, 0, 0));
+            }
+        
+        }
+    }
+
+    public void StopSpawning()
+    {
+        if(Player.GetComponent<PlayerWASD>().isAlive == false)
+        {
+            timer = 100f;
+        }
+    }
+
+    public void ChooseSide()
+    {
+        int sideChance = Random.Range(1, 101);
+        
+        
+        
+        if(sideChance <= 50)
+        {
+            
+        }
+        else
+        {
+          
+        }
+        
         
     }
 }
