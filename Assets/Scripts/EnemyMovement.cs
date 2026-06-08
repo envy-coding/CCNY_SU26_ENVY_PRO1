b00217914fb7618567ae905a0e5d3ad9a6126ebe
Envy <middleton.design@outlook.com>
2026-06-08 11:53:31 -0400

    Enemy Variable Speeds
    Variable Speeds done
    
    TODO
    ENEMY COIN FLIP FACEPOSITION

--- Assets/Scripts/EnemyMovement.cs
+++ Assets/Scripts/EnemyMovement.cs
@@ -9,22 +9,49 @@ public class EnemyMovement : MonoBehaviour
 
     public GameObject Player;
     public bool PlayerAlive;
-    public float speed = 10f;
+    public float speed;
+    private GameObject smallFish;
+    private GameObject mediumFish;
+    private GameObject largeFish;
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
-    { PlayerAlive = true;
+    { 
+      PlayerAlive = true;
       isMoving = true;
-        //enemyTR = this.gameObject.transform;
-        enemyRB = GetComponent<Rigidbody2D>();
-        //enemyTR = GetComponent<Transform>();
+      //enemyTR = this.gameObject.transform;
+      enemyRB = GetComponent<Rigidbody2D>();
+      //enemyTR = GetComponent<Transform>();
+      
+      smallFish = GameObject.Find("SmallEnemy");  
+      mediumFish = GameObject.Find("MediumEnemy");
+      largeFish = GameObject.Find("LargeFish");
     }
 
     // Update is called once per frame
     void Update()
       {
+        Speed();
         OnPlayerDeath();
         //enemyTR.Translate(Vector3.right * speed * Time.deltaTime);
       }
+
+    void Speed()
+  {
+      if (this.gameObject == smallFish)
+      {
+        speed = 2f;
+      }
+
+      else if (this.gameObject == mediumFish)
+      {
+        speed = 5f;
+      }
+
+      else 
+      {
+        speed = 8f;
+      }
+  } 
 
     void FixedUpdate()
     {
